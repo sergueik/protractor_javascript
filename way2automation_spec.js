ff93d2fe52600dc099a0aebf711591cb74b640a9
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2016-05-06 18:20:41 -0400

    intermediate revision - first successul way2automation test

--- way2automation_spec.js
+++ way2automation_spec.js
@@ -1,4 +1,4 @@
-describe('Protractor Demo For Thomas Cook site', function() {
+describe('Protractor practice website - Banking App', function() {
   var customerLogin = element(by.buttonText('Customer Login'));
   var customerLogin2 = element(by.xpath("//button[contains(.,'Customer Login')]"));
   var customerId = element(by.model("custId"));
@@ -8,19 +8,23 @@ describe('Protractor Demo For Thomas Cook site', function() {
     browser.get('http://www.way2automation.com/angularjs-protractor/banking');
   });
 
- it('should land on Protractor practice website - Banking App', function() {
+  it('should land on Protractor practice website - Banking App', function() {
     expect(browser.getTitle()).toEqual('Protractor practice website - Banking App');
   });
 
-  it('should find the Customer Login', function() {
+  it('should find the Customer Login via regular Selenium locator', function() {
     expect(customerLogin2.isDisplayed()).toBeTruthy();
     expect(customerLogin2.getText()).toEqual( "Customer Login");
   });
 
-  it('should offer the Customer Login', function() {
+  it('should find the Customer Login via Protractor Button Text', function() {
+    expect(customerLogin.isDisplayed()).toBeTruthy();
+    expect(customerLogin.getText()).toEqual( 'Customer Login');
+  });
+
+  it('should offer the Customer ID to Login', function() {
     customerLogin2.click();
     expect(customerId.isDisplayed()).toBeTruthy();
   });
 
-  
 });
