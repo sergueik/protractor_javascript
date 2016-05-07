603f4fc4cd6b73c0eee2a407ef5349f783267e62
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2016-05-07 00:40:05 -0400

    intermediate revision - added few expectations and console log

--- way2automation_spec.js
+++ way2automation_spec.js
@@ -1,13 +1,24 @@
+'use strict';
 describe('Protractor practice website - Banking App', function() {
-  
+
   describe ("Customer Login", function() {
-    var customerButton = element(by.buttonText('Customer Login'));
-    var customerButton2 = element(by.xpath("//button[contains(.,'Customer Login')]"));
+    var customerButton2 = element(by.buttonText('Customer Login'));
+    var customerButton = element(by.xpath("//button[contains(.,'Customer Login')]"));
     var customerId = element(by.model('custId'));
     var customers = element.all(by.repeater('cust in Customers'));
     var loginButton = element(by.buttonText('Login'));
     var greeting = element(by.binding('user'));
-    
+    var accountNo =  element(by.binding('accountNo'));
+    // You can also use a substring for a partial match
+    var balance =  element(by.binding('amount'));
+    var currency =  element(by.binding('currency'));
+    /*
+    // A Jasmine spec timed out. Resetting the WebDriver Control Flow.
+    beforeEach(function() {
+      originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+      jasmine.DEFAULT_TIMEOUT_INTERVAL = 60000;
+    });
+    */
     beforeEach(function() {
       browser.driver.manage().window().maximize();
       browser.get('http://www.way2automation.com/angularjs-protractor/banking');
@@ -34,16 +45,30 @@ describe('Protractor practice website - Banking App', function() {
       expect(customerId.getAttribute('id')).toEqual('userSelect');
       expect(customers.count()).toEqual(5);
       });
-      
+
     it('should greet the Logged-in Customer', function() {
       customerButton.click();
       var first_customer = customers.get(1);
-      first_customer.click();
       var userName = first_customer.getText();
+      first_customer.getText().then(function(value) {
+        console.log('Customer: ' + value.toString());
+      });
+      first_customer.click();
       expect(loginButton.isDisplayed()).toBeTruthy();
       loginButton.click();
-      expect(greeting.getText()).toEqual( userName );      
+      expect(greeting.getText()).toEqual( userName );
+      expect(accountNo.getText()).toMatch(/\d{4}/);
+      accountNo.getText().then(function(value) {
+        console.log('accountNo: ' + value.toString());
+      });
+      expect(balance.getText()).toMatch("\\d+");
+      balance.getText().then(function(value) {
+        console.log('Balance: ' + value );
+      });
+      expect(currency.getText()).toMatch(/(?:Dollar|Pound|Rupee)/);
+      currency.getText().then(function(value) {
+        console.log('Currency: ' + value.toString());
+      });
     });
   });
-
 });
