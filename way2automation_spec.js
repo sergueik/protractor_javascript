90a121d959a7ab3810c272ea391e182a2f857598
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2016-05-06 23:52:24 -0400

    intermediate revision - ported ShouldLoginCustomer way2automation test from c#

--- way2automation_spec.js
+++ way2automation_spec.js
@@ -1,30 +1,49 @@
 describe('Protractor practice website - Banking App', function() {
-  var customerLogin = element(by.buttonText('Customer Login'));
-  var customerLogin2 = element(by.xpath("//button[contains(.,'Customer Login')]"));
-  var customerId = element(by.model("custId"));
+  
+  describe ("Customer Login", function() {
+    var customerButton = element(by.buttonText('Customer Login'));
+    var customerButton2 = element(by.xpath("//button[contains(.,'Customer Login')]"));
+    var customerId = element(by.model('custId'));
+    var customers = element.all(by.repeater('cust in Customers'));
+    var loginButton = element(by.buttonText('Login'));
+    var greeting = element(by.binding('user'));
+    
+    beforeEach(function() {
+      browser.driver.manage().window().maximize();
+      browser.get('http://www.way2automation.com/angularjs-protractor/banking');
+      browser.waitForAngular();
+    });
 
-  beforeEach(function() {
-	browser.driver.manage().window().maximize();
-    browser.get('http://www.way2automation.com/angularjs-protractor/banking');
-  });
+    it('should land on Protractor practice website - Banking App', function() {
+      expect(browser.getTitle()).toEqual('Protractor practice website - Banking App');
+    });
 
-  it('should land on Protractor practice website - Banking App', function() {
-    expect(browser.getTitle()).toEqual('Protractor practice website - Banking App');
-  });
+    it('should find the Customer Login via regular Selenium locator', function() {
+      expect(customerButton2.isDisplayed()).toBeTruthy();
+      expect(customerButton2.getText()).toEqual( 'Customer Login');
+    });
 
-  it('should find the Customer Login via regular Selenium locator', function() {
-    expect(customerLogin2.isDisplayed()).toBeTruthy();
-    expect(customerLogin2.getText()).toEqual( "Customer Login");
-  });
+    it('should find the Customer Login via Protractor Button Text', function() {
+      expect(customerButton.isDisplayed()).toBeTruthy();
+      expect(customerButton.getText()).toEqual( 'Customer Login');
+    });
 
-  it('should find the Customer Login via Protractor Button Text', function() {
-    expect(customerLogin.isDisplayed()).toBeTruthy();
-    expect(customerLogin.getText()).toEqual( 'Customer Login');
-  });
-
-  it('should offer the Customer ID to Login', function() {
-    customerLogin2.click();
-    expect(customerId.isDisplayed()).toBeTruthy();
+    it('should offer selectable Customer Logins', function() {
+      customerButton.click();
+      expect(customerId.isDisplayed()).toBeTruthy();
+      expect(customerId.getAttribute('id')).toEqual('userSelect');
+      expect(customers.count()).toEqual(5);
+      });
+      
+    it('should greet the Logged-in Customer', function() {
+      customerButton.click();
+      var first_customer = customers.get(1);
+      first_customer.click();
+      var userName = first_customer.getText();
+      expect(loginButton.isDisplayed()).toBeTruthy();
+      loginButton.click();
+      expect(greeting.getText()).toEqual( userName );      
+    });
   });
 
 });
