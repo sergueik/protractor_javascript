940049dd5159ee9bc542c7c5b1e03552adaf3b3a
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2016-05-10 07:43:56 -0400

    intermediate revision - datepicker spec snapshot

--- conf.js
+++ conf.js
@@ -2,8 +2,8 @@
 exports.config = {
   framework: 'jasmine',
   seleniumAddress: 'http://127.0.0.1:4444/wd/hub',
-//  specs: ['spec/way2automation_spec.js', 'spec/qualityshepherd_spec.js'],
-  specs: ['spec/qualityshepherd_spec.js'],
+//  specs: ['spec/way2automation_spec.js','spec/datepicker_spec.js', 'spec/qualityshepherd_spec.js'],
+  specs: ['spec/*_spec.js'],
   capabilities: {
     browserName: 'chrome',
     shardTestFiles: true,
--- spec/qualityshepherd_spec.js
+++ spec/qualityshepherd_spec.js
@@ -1,18 +1,20 @@
 'use strict';
 describe('Protractor practice website - Friends Page', function() {
 
-  describe ('Friends Page', function() {
-    
+  describe('Friends Page', function() {
+
     var searchBox = element(by.model('search'));
     var addnameBox = element(by.model('addName'));
     var addButton = element(by.buttonText('+ add'));
     var actualCount = $('em.ng-binding');
     var deleteButton = $('i.icon-trash');
     var deleteButtons = $$('i.icon-trash');
-    var friendName = function(name) { return element.all(by.cssContainingText('td.ng-binding', name)); };
+    var friendName = function(name) {
+      return element.all(by.cssContainingText('td.ng-binding', name));
+    };
     var friends = element.all(by.repeater('row in rows'));
     var friendNames = element.all(by.repeater('row in rows').column('{{row}}'));
-    
+
     beforeEach(function() {
       browser.get('http://qualityshepherd.com/angular/friends/');
       browser.waitForAngular();
@@ -21,22 +23,23 @@ describe('Protractor practice website - Friends Page', function() {
     // http://stackoverflow.com/questions/34320148/how-can-i-inspect-the-contents-of-a-dropdown-list-with-protractor?lq=1
     // http://stackoverflow.com/questions/22806379/angularjs-protractor-sum-all-row-values-in-repeater
     it('should add a friend', function() {
+      var firstFriend = friendNames.get(0); // [object Object]
+      expect(firstFriend.getText()).toEqual('Paul');
       friends.then(function(rows) {
         var friendsCount = rows.length;
         var newFriendName = 'George';
         addnameBox.sendKeys(newFriendName);
-        addButton.click().then( function() {
-          expect(friends.count()).toEqual(friendsCount + 1); 
-        });
+        addButton.click();
+        expect(friends.count()).toEqual(friendsCount + 1);
       });
-    
       var newFriendName = 'Ringo';
       addnameBox.sendKeys(newFriendName);
-      addButton.click().then( function() {
+      addButton.click().then(function() {
         friendNames.last().getText().then(function(value) {
-          console.log('Last Friend Name: ' + value );
+          console.log('Last Friend Name: ' + value);
         });
       });
+      // https://github.com/andresdominguez/protractor-meetup/blob/master/test/e2e/member3-spec.js
     });
   });
 });
