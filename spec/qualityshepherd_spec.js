c82af0689f15e4761f38a54c6ea30edcaef84b2d
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2016-05-09 21:57:32 -0400

    intermediate revision - cleanup and simplify

--- spec/qualityshepherd_spec.js
+++ spec/qualityshepherd_spec.js
@@ -19,40 +19,23 @@ describe('Protractor practice website - Friends Page', function() {
     });
 
     // http://stackoverflow.com/questions/34320148/how-can-i-inspect-the-contents-of-a-dropdown-list-with-protractor?lq=1
-    // http://stackoverflow.com/questions/34320148/how-can-i-inspect-the-contents-of-a-dropdown-list-with-protractor?lq=1
-    it('should add a friend', function() {  
-      var friendNamesArray = element.all(by.repeater('row in rows').column('{{row}}')).map(function(row) {
-        return row.getText();
-      });
-      friendNamesArray.then(function(value) {
-        expect(value.length).toEqual(3);
-        var lengthCheck = value.length;
-        // console.log('Length Check(in) : ' + lengthCheck );
+    // http://stackoverflow.com/questions/22806379/angularjs-protractor-sum-all-row-values-in-repeater
+    it('should add a friend', function() {
+      friends.then(function(rows) {
+        var friendsCount = rows.length;
         var newFriendName = 'George';
         addnameBox.sendKeys(newFriendName);
         addButton.click().then( function() {
-          expect(friends.count()).toEqual(lengthCheck + 1); 
+          expect(friends.count()).toEqual(friendsCount + 1); 
         });
       });
     
       var newFriendName = 'Ringo';
       addnameBox.sendKeys(newFriendName);
       addButton.click().then( function() {
-        var last_friend = friendNames.last();
-        last_friend.getText().then(function(value) {
-          console.log('Last Friend : ' + value.toString());
+        friendNames.last().getText().then(function(value) {
+          console.log('Last Friend Name: ' + value );
         });
-      });
-      
-      var friendNamesString = element.all(by.repeater('row in rows').column('{{row}}')).map(function(row) {
-        return row.getText();
-      }).then(function(arr) {
-        return arr.reduce(function(a, b) {
-          return a.toString() + '=' + b.toString();
-        })
-      });
-      friendNamesString.then(function(value) {
-        expect(value.split(/=/).length).toEqual(5);
       });
     });
   });
